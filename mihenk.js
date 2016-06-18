8aefcb7a6f45ba597d99988aaf8c6c721ed6fd1b
Guvenir <omerguvenir@yandex.com>
2016-06-18 11:14:04 +0300

    bubble added

--- mihenk.js
+++ mihenk.js
@@ -20,6 +20,7 @@ var mihenk = {};
 var dset = [];
 var xdata;
 var ydata;
+var yaricap;
 var ranges = [
     {
         "x_labels": ["1", "2", "3", "4", "6", "8"],
@@ -80,6 +81,17 @@ mihenk.line = function(veri) {
     grafik = veri.opt;
     if (dset !== undefined) {
         line();
+    }
+}
+
+mihenk.bubble = function(veri){
+    xdata = veri.xdata;
+    ydata = veri.ydata;
+    convert(xdata, ydata);
+    yaricap = veri.r;
+    grafik = veri.opt;
+    if (dset !== undefined) {
+        bubble();
     }
 }
 
@@ -1446,6 +1458,261 @@ function line() {
     }
 }
 
+function bubble(){
+    //
+    var xmin, xmax, ymin, ymax;
+    xmin = dset[0].x;
+    xmax = dset[0].x;
+    ymin = dset[0].y;
+    ymax = dset[0].y;
+
+    for (t = 0; t < dset.length; t++) {
+        if (dset[t].x < xmin) {
+            xmin = dset[t].x;
+        }
+        if (dset[t].y < ymin) {
+            ymin = dset[t].y;
+        }
+        if (dset[t].x > xmax) {
+            xmax = dset[t].x;
+        }
+        if (dset[t].y > ymax) {
+            ymax = dset[t].y;
+        }
+    }
+    //console.log(xmin, xmax, '|', ymin, ymax);
+    ranges[0].x_range = [xmin - 1, xmax + 1];
+    ranges[0].y_range = [ymin - 1, ymax + 1];
+    //
+    var margin = { top: 10, right: 90, bottom: 90, left: 90 },
+        width = 700.51 + margin.left + margin.right,
+        height = 500.51 - margin.top - margin.bottom;
+    var c1, c2, c3, c4, c5;
+
+    var format = d3.format(".2f");
+
+    var x1 = d3.scale.ordinal()
+        .domain(ranges[0]["x_range"])
+        .rangeRoundBands([0, width], .1);
+
+    var x = d3.scale.linear()
+        .domain(ranges[0]["x_range"])
+        .range([0, width]);
+
+    var y = d3.scale.linear()
+        .domain(ranges[0]["y_range"])
+        .range([0, height]);
+
+    var z = d3.scale.category20();
+
+    //y ekseni için scale
+    var y2 = d3.scale.linear()
+        .domain(ranges[0]["y_range"])
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .innerTickSize(-height)
+        .outerTickSize(0)
+        //.tickFormat(function(d,i){return ranges[0]["x.labels"][i]})
+        .tickPadding(10);
+
+    var yAxis = d3.svg.axis()
+        .scale(y2)
+        .orient("left")
+        .innerTickSize(-width)
+        .outerTickSize(0)
+        //.tickFormat(function(d,i){return ranges[0]["y.labels"][i]})
+        .tickPadding(10);
+
+    var zoom = d3.behavior.zoom()
+    				.x(x)
+    				.y(y2)
+    				.scaleExtent([1, 5])
+    				.on("zoom", zoomed);
+    d3.select("#" + grafik.div_id).select("svg").remove();
+    var svg = d3.select("#"+grafik.div_id).append("svg")
+        //.attr("width", width + margin.left + margin.right)
+        //.attr("height", height + margin.top + margin.bottom)
+        .attr("viewBox", "0 0 " + (width + margin.right + margin.left) + " " + (height + margin.top + margin.bottom))
+        .append("svg:g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    //.call(d3.behavior.zoom().x(x).y(y2).scaleExtent([1, 10]).on("zoom", zoomed));
+
+    svg.append("g")
+        .attr("class", "y axis")
+        //.tickFormat(ranges[0]["y.labels"])
+        .call(yAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -80)
+        .attr("dy", ".71em")
+        .attr("x", -height / 3)
+        .style("text-anchor", "end")
+        .text(grafik.ylab);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("x", width / 2 + 10)//xAxias deki label değerinin yeri için buraya bak
+        .attr("y", 60)
+        .style("text-anchor", "end")
+        .text(grafik.xlab);//Json üzerinden bu değeri değiştir 
+
+    var y3 = d3.scale.linear()
+        .domain(ranges[0]["y_range"])
+        .range([height, 0]);
+    //Zoom ve label ların ayarlanması    
+    if (grafik.zoom === true && grafik.x_labels.length === 0 && grafik.y_labels.length === 0) {
+        //console.log(grafik.x_labels.length, grafik.y_labels.length);
+        svg.call(zoom);
+    } else {
+        //console.log(grafik.x_labels.length, xdata.length);
+        if (grafik.x_labels.length !== 0) {
+            xAxis.tickFormat(function(d, i) { return grafik.x_labels[i]; });
+            svg.select(".x.axis").call(xAxis);
+        }
+        if (grafik.y_labels.length !== 0) {
+            yAxis.tickFormat(function(d, i) { return grafik.y_labels[i]; });
+            svg.select(".y.axis").call(yAxis);
+        }
+    }
+    var svg2 = svg.append("svg").attr("height", height).attr("width", width);
+
+    var xval, yval;
+    var xset = new Array, xlset = new Array;
+    var sss = svg2.append("text");
+    var ttt = svg2.append("text");
+    var rrr = svg2.append("rect");
+    for (var f = 0; f < dset.length; f++) {
+        xset.push(x1(dset[f].x));
+    }
+
+    var circles = svg2.selectAll("circle")
+        .data(dset)
+        .enter()
+        .append("circle")
+        .attr("id", function(d, i) { return "c" + i; })
+        .attr("cx", function(d) { return x(d.x); })
+        .attr("cy", function(d) { return height - y(d.y); })
+        .attr("opacity", 1)
+        .attr("r", function(d,i){
+            return yaricap[i];
+        })
+        .attr("fill", function(d, i) {
+            if (grafik.color.length !== 0) {
+                if (grafik.color.length < xdata.length) {
+                    return grafik.color[i % grafik.color.length];
+                } else {
+                    return grafik.color[i];
+                }
+
+            } else {
+                return z(i);
+            }
+        })
+        .on("mouseover", over)
+        .on("mouseout", out);
+
+    /*Çıktı için gerekli düzenlemeler*/
+    d3.selectAll(".tick")
+        .attr("font", "15px sans-serif")
+        .attr("fill", "#333")
+        //.attr("stroke", "#333")
+        .attr("shape-rendering", "crispEdges")
+        .attr("opacity", 0.1);
+
+    d3.selectAll(".axis.text")
+        .attr("text-anchor", "end")
+        .attr("font", "12px Arial")
+
+    d3.selectAll("text")
+        .attr("fill", "#777");
+
+    d3.selectAll(".tick line")
+        //.style("stroke-dasharray", ("5, 5"))
+        .attr("stroke", "#333")
+        .attr("opacity", "0.1")
+
+    d3.select(".x.axis path")
+        .attr("fill", "none")
+        .attr("stroke", "#6B6B6B")
+        .attr("shape-rendering", "crispEdges")
+
+    d3.select(".y.axis path")
+        .attr("fill", "none")
+        .attr("stroke", "#6B6B6B")
+        .attr("shape-rendering", "crispEdges")
+    d3.selectAll(".domain")
+        .attr("fill", "none")
+        .attr("stroke", "#6B6B6B")
+        .attr("shape-rendering", "crispEdges")
+    /*--------------------------------*/
+    //tooltip
+    var div = d3.select("#"+grafik.div_id).append("div")
+        .attr("class", "bubble tooltip")
+        .style("opacity", 0);
+    div.append('div')
+        .attr('class', 'labell');
+    div.append('div')
+        .attr('class', 'count');
+    div.append('div')
+        .attr('class', 'percent');
+    // hover fonksiyonları
+    function over(d, i, j) {
+        d3.select(this)
+            .transition(500)
+            .attr("r", function(){
+                return yaricap[i]+2;
+            });
+        div.transition()
+            .duration(500)
+            .style("opacity", 1);
+        div.select('.labell').html(grafik.ylab+" :" + format(d.y));
+        div.select('.count').html(grafik.xlab+" :" + format(d.x));
+        var mouseCoords = d3.mouse(
+            d3.select("div.bubble.tooltip").node().parentElement);
+        div.transition()
+            .duration(500)
+            .style("opacity", 0.9)
+            .style("display", "block")
+            .style("left", mouseCoords[0] - 30 + "px")
+            .style("top", mouseCoords[1] - 80 + "px");
+    }
+
+
+    function out(d, i, j) {
+        d3.select(this)
+            .transition(500)
+            .attr("r", function(){
+                return yaricap[i];
+            });
+        //.attr("stroke","#333");
+
+        div.transition()
+            .duration(500)
+            .style("opacity", 0);
+
+        svg2.selectAll(".test").attr("opacity", 0).style("display", "none");
+        svg2.selectAll(".circles").attr("opacity", 0).style("display", "none");
+
+    }
+
+    function zoomed() {
+        svg.select(".x.axis").call(xAxis);
+        svg.select(".y.axis").call(yAxis);
+
+        circles.attr("transform", "translate(" + d3.event.translate[0] + ",0)scale(" + d3.event.scale + ",1)");
+        circles.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+        d3.selectAll("#lline").attr("transform", "translate(" + d3.event.translate[0] + ",0)scale(" + d3.event.scale + ",1)");
+        d3.selectAll("#lline").attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+    }
+}
 
 //Verilerin uygun formata getirilmesi
 function convert(xdata, ydata) {
